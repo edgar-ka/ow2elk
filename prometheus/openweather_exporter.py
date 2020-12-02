8aa15ee979fde355fc340ddfd5d8aa64dd50f40b
Edgar Kalaev <edka@mail.ru>
2020-12-02 22:49:17 +0300

    working python pusher. needs work

--- prometheus/openweather_exporter.py
+++ prometheus/openweather_exporter.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-import os
+import os, sys
 import urllib.request as Rq
-import json
+import json, yaml
 import pprint
 
 pp = pprint.PrettyPrinter()
 
 API_TOKEN_FILE = '.token'
-INDEX_NAME = 'o_weather_valday'
 CITY_ID = 477301
+CONFIG_FILE = '/etc/openweathermap_export/ow_config.yml'
 
-weather_url = "https://api.openweathermap.org/data/2.5/weather?id={city}&APPID={token}&units=metric"
+with open(CONFIG_FILE, 'r') as c:
+    config = yaml.load(c, Loader=yaml.CLoader)
+
+pp.pprint(config)
+
+weather_url = "https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={token}&units=metric"
 pushgateway_url = 'http://localhost:9091/metrics/job/openweather_exporter'
 
 def get_token():
@@ -28,11 +33,13 @@ def get_token():
         return None
 
 ## try to get API token
-OW_API_TOKEN = get_token()
+# OW_API_TOKEN = get_token()
+OW_API_TOKEN = config['api_token']
+CITY = config['cities'][0]  # for now
 
 if OW_API_TOKEN:
     ## get current weather
-    with Rq.urlopen(weather_url.format(city=CITY_ID, token=OW_API_TOKEN)) as f:
+    with Rq.urlopen(weather_url.format(city=CITY, token=OW_API_TOKEN)) as f:
         msg = json.loads(f.read())
 
     ## document computation
@@ -64,29 +71,38 @@ if OW_API_TOKEN:
     city_name = msg['name']
     
     elastic_document = (
-    f'''weather_info{{location="{city_name}",country="{msg['sys']['country']}",weatherid="{msg['weather'][0]['id']}",weathermain="{msg['weather'][0]['main']}",weatherdesc="{msg['weather'][0]['description']}"}} {msg['id']}\n'''
+    '''# TYPE weather_info gauge\n'''
+     f'''weather_info{{location="{city_name}",country="{msg['sys']['country']}",weatherid="{msg['weather'][0]['id']}",weathermain="{msg['weather'][0]['main']}",weatherdesc="{msg['weather'][0]['description']}"}} {msg['id']}\n'''
+    '''# TYPE weather_return_code gauge\n'''
     f'''weather_return_code{{name="{city_name}"}} {msg['cod']}\n'''
+    '''# TYPE weather_temperature gauge\n'''
     f'''weather_temperature{{name="{city_name}",type="current"}} {msg['main']['temp']}\n'''
+    '''# TYPE weather_humidity gauge\n'''
     f'''weather_humidity{{name="{city_name}"}} {msg['main']['humidity']}\n'''
-    f'''weather_pressure{{name="{city_name}",level="ground"}} {msg['main']['grnd_level'] / 1.33322387415}\n'''
-    f'''weather_pressure{{name="{city_name}",level="sea"}} {msg['main']['sea_level'] / 1.33322387415}\n'''
+    '''# TYPE weather_pressure gauge\n'''
+    f'''weather_pressure{{name="{city_name}",level="ground"}} {msg['main']['grnd_level']}\n'''
+    f'''weather_pressure{{name="{city_name}",level="sea"}} {msg['main']['sea_level']}\n'''
+    '''# TYPE weather_wind_speed gauge\n'''
     f'''weather_wind_speed{{name="{city_name}"}} {msg['wind']['speed']}\n'''
+    '''# TYPE weather_wind_direction gauge\n'''
     f'''weather_wind_direction{{name="{city_name}"}} {msg['wind']['deg']}\n'''
+    '''# TYPE weather_clouds_percent gauge\n'''
     f'''weather_clouds_percent{{name="{city_name}"}} {msg['clouds']['all']}\n'''
+    '''# TYPE weather_sun_epoch gauge\n'''
     f'''weather_sun_epoch{{name="{city_name}",change="sunrise"}} {msg['sys']['sunrise']}\n'''
     f'''weather_sun_epoch{{name="{city_name}",change="sunset"}} {msg['sys']['sunset']}\n'''
     )
-
+    pp.pprint(elastic_document)
     ## preparing binary data for urllib
-    # jdata = json.dumps(elastic_document).encode('utf-8')
+    jdata = elastic_document.encode('utf-8')
 
     ## posting document
-    # elastic_rq = Rq.Request(pushgateway_url, data=jdata, method='POST')
-    # elastic_rq.add_header('Content-Type', 'application/json; charset=utf-8')
-    # with Rq.urlopen(elastic_rq) as f:
-    #     resp = json.loads(f.read())
+    elastic_rq = Rq.Request(pushgateway_url, data=jdata, method='POST')
+    elastic_rq.add_header('Content_Type', 'form-data')
+    with Rq.urlopen(elastic_rq) as f:
+        resp = f.read()
 
-    print(elastic_document)
+    # pp.pprint(resp)
 else:
     # TODO: implement some notification
     print("Couldn't get API token!")
