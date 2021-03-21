414648bcf767095aed1cc11696ac88966efe763b
Edgar Kalaev <edka@mail.ru>
2021-03-21 21:34:32 +0300

    FIX pushgw keeps only last POST

--- prometheus/openweather_exporter.py
+++ prometheus/openweather_exporter.py
@@ -19,6 +19,19 @@ OW_API_TOKEN = config['api_token']
 CITIES = config['cities']  # for now
 PUSHGW_ENDPOINT = config["pushgw_endpoint"]["hostname"] # FIXME set default to localhost?
 PUSHGW_PORT = config["pushgw_endpoint"]["port"]         # FIXME set default 9091
+elastic_document = (
+'''# TYPE weather_info gauge\n'''
+'''# TYPE weather_return_code gauge\n'''
+'''# TYPE weather_temperature gauge\n'''
+'''# TYPE weather_humidity gauge\n'''
+'''# TYPE weather_pressure gauge\n'''
+'''# TYPE weather_wind_speed gauge\n'''
+'''# TYPE weather_wind_direction gauge\n'''
+'''# TYPE weather_clouds_percent gauge\n'''
+'''# TYPE weather_sun_epoch gauge\n'''
+'''# TYPE weather_rain_1h gauge\n'''
+'''# TYPE weather_snow_1h gauge\n'''
+)
 
 for city in CITIES:
     ## get current weather
@@ -27,40 +40,29 @@ for city in CITIES:
 
     city_name = msg['name']
     
-    elastic_document = (
-    '''# TYPE weather_info gauge\n'''
-     f'''weather_info{{location="{city_name}",country="{msg['sys']['country']}",weatherid="{msg['weather'][0]['id']}",weathermain="{msg['weather'][0]['main']}",weatherdesc="{msg['weather'][0]['description']}"}} {msg['id']}\n'''
-    '''# TYPE weather_return_code gauge\n'''
+    elastic_document += (
+    f'''weather_info{{location="{city_name}",country="{msg['sys']['country']}",weatherid="{msg['weather'][0]['id']}",weathermain="{msg['weather'][0]['main']}",weatherdesc="{msg['weather'][0]['description']}"}} {msg['id']}\n'''
     f'''weather_return_code{{name="{city_name}"}} {msg['cod']}\n'''
-    '''# TYPE weather_temperature gauge\n'''
     f'''weather_temperature{{name="{city_name}",type="current"}} {msg['main']['temp']}\n'''
-    '''# TYPE weather_humidity gauge\n'''
     f'''weather_humidity{{name="{city_name}"}} {msg['main']['humidity']}\n'''
-    '''# TYPE weather_pressure gauge\n'''
     f'''weather_pressure{{name="{city_name}",level="ground"}} {msg.get('main', {}).get('grnd_level', msg['main']['pressure'])}\n'''
     f'''weather_pressure{{name="{city_name}",level="sea"}} {msg.get('main', {}).get('sea_level', msg['main']['pressure'])}\n'''
-    '''# TYPE weather_wind_speed gauge\n'''
     f'''weather_wind_speed{{name="{city_name}"}} {msg['wind']['speed']}\n'''
-    '''# TYPE weather_wind_direction gauge\n'''
     f'''weather_wind_direction{{name="{city_name}"}} {msg['wind']['deg']}\n'''
-    '''# TYPE weather_clouds_percent gauge\n'''
     f'''weather_clouds_percent{{name="{city_name}"}} {msg['clouds']['all']}\n'''
-    '''# TYPE weather_sun_epoch gauge\n'''
     f'''weather_sun_epoch{{name="{city_name}",change="sunrise"}} {msg['sys']['sunrise']}\n'''
     f'''weather_sun_epoch{{name="{city_name}",change="sunset"}} {msg['sys']['sunset']}\n'''
-    '''# TYPE weather_rain_1h gauge\n'''
     f'''weather_rain_1h{{name="{city_name}"}} {msg.get('rain', {}).get('1h', 0)}\n'''
-    '''# TYPE weather_snow_1h gauge\n'''
     f'''weather_snow_1h{{name="{city_name}"}} {msg.get('snow', {}).get('1h', 0)}\n'''
     )
     ## TODO add coordinates
     ## TODO add weather picture id
 
-    ## preparing binary data for urllib
-    jdata = elastic_document.encode('utf-8')
+## preparing binary data for urllib
+jdata = elastic_document.encode('utf-8')
 
-    ## posting document
-    elastic_rq = Rq.Request(pushgateway_url.format(host=PUSHGW_ENDPOINT, port=PUSHGW_PORT), data=jdata, method='POST')
-    elastic_rq.add_header('Content_Type', 'form-data')
-    with Rq.urlopen(elastic_rq) as f:
-        resp = f.read()
+## posting document
+elastic_rq = Rq.Request(pushgateway_url.format(host=PUSHGW_ENDPOINT, port=PUSHGW_PORT), data=jdata, method='POST')
+elastic_rq.add_header('Content_Type', 'form-data')
+with Rq.urlopen(elastic_rq) as f:
+    resp = f.read()
